c0b616ad7042df575f4eb0b33294e539d1212492
garciamendes <garciaprog0101@gmail.com>
2024-12-15 14:52:00 -0300

    Endpoint para a listagem de avaliações do user atual

--- api-book-wise/src/main/java/com/pex/api_book_wise/repositories/BookRatingRepository.java
+++ api-book-wise/src/main/java/com/pex/api_book_wise/repositories/BookRatingRepository.java
@@ -1,11 +1,15 @@
 package com.pex.api_book_wise.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.pex.api_book_wise.domains.book_rating.BookRating;
 
-public interface BookRatingRepository extends JpaRepository<BookRating, UUID> {
-
+public interface BookRatingRepository extends JpaRepository<BookRating, UUID>, JpaSpecificationExecutor<BookRating> {
+  @SuppressWarnings("null")
+  List<BookRating> findAll(Specification<BookRating> specification);
 }
