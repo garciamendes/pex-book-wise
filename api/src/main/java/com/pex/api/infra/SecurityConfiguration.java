42f48bf4ad952054a37ad5c3f8f6c5bfa506344b
garciamendes <garciaprog0101@gmail.com>
2025-01-05 15:39:26 -0300

    alteração na permissões

--- api/src/main/java/com/pex/api/infra/SecurityConfiguration.java
+++ api/src/main/java/com/pex/api/infra/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/error").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/*").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
